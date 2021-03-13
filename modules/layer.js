86ae60c5cc3ae198145918697e204764c5b6eac6
ThreePixDroid <51239861+ThreePixDroid@users.noreply.github.com>
2021-03-14 01:11:24 +0300

    Update layer.js

--- modules/layer.js
+++ modules/layer.js
@@ -1,19 +1,14 @@
 export class Layer {
-  constructor(container) {                         
-   container.appendChild(this.createLayer());      //put Canvas to Container
+  constructor(container = document.body) {          
+    this.canvas = document.createElement( `canvas` );//create new Canvas element
+    this.context = this.canvas.getContext( `2d` );   //get access to 2d drawing tools    
+    container.appendChild( this.canvas );      //put Canvas to Container
 
-   addEventListener(`resize`, () => this.fitToContainer(), false);
-   this.fitToContainer();                        
-  }
-  createLayer() {
-    this.canvas = document.createElement(`canvas`);//create new Canvas element
-    this.context = this.canvas.getContext(`2d`);   //get access to 2d drawing tools
-    return this.canvas;                                 
+    addEventListener(`resize`, () => this.fitToContainer(), false);
+    this.fitToContainer();          
   }
   fitToContainer() {                                //fit Canvas size to container
-    this.canvas.width = this.canvas.offsetWidth;
-    this.canvas.height = this.canvas.offsetHeight;
+    this.w = this.canvas.width = this.canvas.offsetWidth;
+    this.h = this.canvas.height = this.canvas.offsetHeight;
   }
-  get w() { return this.canvas.width }   //get width
-  get h() { return this.canvas.height }  //get height
 }
